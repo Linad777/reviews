41e54bdb60a7551fa6bc54470a3d7e307df973b5
Daniil Vnukov <danyavnukov1033@gmail.com>
2025-10-09 11:56:45 +0300

    Some Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,67 @@
+import people from "./data.js";
+import {useState} from "react";
+
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from "react-icons/fa";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0)
+  const {name, job, image, text} = people[index]
+
+  console.log(1 % 4)
+  console.log(2 % 4)
+  console.log(3 % 4)
+  console.log(4 % 4)
+
+
+  const nextPerson = () => {
+      setIndex((currentIndex)=>{
+        const newIndex = (currentIndex + 1) % people.length;
+
+        return newIndex;
+      })
+  };
+  const prevPerson = () => {
+    setIndex((currentIndex)=>{
+      const newIndex = (currentIndex - 1 + people.length) % people.length;
+
+      return newIndex;
+    })
+  };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    const newIndex = randomNumber % people.length;
+    setIndex(newIndex);
+  }
+
+  return (
+      <main >
+        <article className="review">
+          <div className="img-container">
+            <img src={image} className="person-img" alt={name}/>
+            <span className='quote-icon'>
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h4 className='author'>{name}</h4>
+          <p className='job'>{job}</p>
+          <p className='info'>{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+          <button className="btn btn-hipster" onClick={randomPerson}>
+            suprise me
+          </button>
+        </article>
+      </main>
+  )
+
 };
 export default App;
